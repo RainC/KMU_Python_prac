d25a3135cb2a092270f813cb8bc70ba27acdddf6
DongGyu Kim <support@rainlab.net>
2016-03-30 10:15:05 +0900

    add codes for stars

--- 1-Python_Stars/Stars.py
+++ 1-Python_Stars/Stars.py
@@ -3,3 +3,10 @@ i = 0
 for i in range(1, s_int+1):
     print("*"*i)
     
+
+#s_int = int(input("Input s_int:"))
+#strs = "*"
+#for i in range(s_int):
+#    print(strs, end="")
+#    strs = strs + "*"
+#    print()
