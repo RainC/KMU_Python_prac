bc3635f7195e0f89759a5913ce2ddea0ece6e3e1
rainc <rainc@raincui-MacBook-Air.local>
2016-04-04 09:30:52 +0900

    print type

--- 2-List-append/list.py
+++ 2-List-append/list.py
@@ -1,4 +1,26 @@
+#array management 
 family = ["Mother","Father","Old Brother", "Brother"]
-
+family.append("Me")
+list_for_nummbers = [1,2,3,4,5,6]
 print(family)
+print(family[0])
+print(list_for_nummbers[-1]) # last data of index print(6)
 
+
+print("---------------------------------------------")
+#slice list
+mylist = ["string1","string2","string3","string4"]
+print (mylist[0:1])
+
+numbers = [1,2,3,4,5,6]
+print(numbers[1:4])
+
+#print the type of variable
+
+int_numbers = [1,2,3,4,5,6]
+string = ["dummy_data","Work"]
+print("int_numbers of index 1 type : " , type(numbers[1]))
+print("string variable type : " , type(string))# <class 'list'>
+print("string variable index 3 type " , type(string[0]))# <class 'list'>
+
+    
